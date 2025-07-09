9195ce41a76346531673d13404cf4f1108bb597a
primouomo123 <luismanuelrios123@gmail.com>
2025-07-09 13:15:00 -0400

    I've added the comments

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+//Creation of the object including the username and password
 const userInfo = {
     "username": "user1",
     "password": "password123"
 }
 
+//This is the function to track the login attempts
 function createLoginTracker(userInfo) {
-  let attemptCount = 0;
+  let attemptCount = 0; // This is the counter of attempts
 
+  //This is the arrow function with the passwordAttempt parameter with all the conditionals
   const loginAttempt = (passwordAttempt) => {
     if (passwordAttempt === userInfo.password) {
-      return 'Login successful';
+      return 'Login successful'; //This is the result if the user types the correct password
     } else {
       attemptCount++;
       if (attemptCount <= 3) {
-        return `Attempt ${attemptCount}: Login failed`;
+        return `Attempt ${attemptCount}: Login failed`; //This is to show the count of all failed attempts (up to 3 failed attempts)
         
       }
       if (attemptCount >= 3) {
-        return 'Account locked due to too many failed login attempts'
+        return 'Account locked due to too many failed login attempts' /*This is the final message, in case the user tried 3 times
+        and his account is now locked*/
       }
 
     }
--- interactive-program.js
+++ interactive-program.js
@@ -1,3 +1,4 @@
+//This is the readline interface
 const { stdin, stdout, rawListeners } = require("process");
 
 const readline = require(`readline`).createInterface({
@@ -5,47 +6,52 @@ const readline = require(`readline`).createInterface({
   output: stdout
 });
 
+//Creation of the object including the username and password
 const userInfo = {
   userName: "user1",
   password: "password123"
 }
 
+//This is the creation of the variable to store the password the user will type
 let passwordAttempt
 
+//This is the function to track the login attempts
 function createLoginTracker(userInfo) {
 
     let attemptCount = 0
 
     const loginAttempt = (passwordAttempt) => {
         if (passwordAttempt === userInfo.password) {
-            console.log('Login successful')
+            console.log('Login successful') //This is the result if the user types the correct password
             readline.close();
             return
         }
 
         else {
-            attemptCount++;
+            attemptCount++; //When the password is incorrect, the count starts increasing by 1
             if (attemptCount < 3) {
-                console.log(`Attempt ${attemptCount}: Login failed`)
-                readline.question(`Please enter password again`, (inputPassword) => {
-                    passwordAttempt = inputPassword;
-                    loginAttempt(passwordAttempt);
+                console.log(`Attempt ${attemptCount}: Login failed`) //This is to show the count of all failed attempts (up to 2 failed attempts)
+                readline.question(`Please enter password again`, (inputPassword) => { //This is the question to let the user retype the password
+                    passwordAttempt = inputPassword; //I save the user password in the variable named passwordAttempt
+                    loginAttempt(passwordAttempt); /*I call the arrow function again to repeat the process until the user types the right
+                    password or until the account is locked after 3 failed times.*/
                 })
             }
             
             else if (attemptCount >= 3) {
-                console.log(`Attempt ${attemptCount}: Login failed`)
-                console.log('Account locked due to too many failed login attempts')
+                console.log(`Attempt ${attemptCount}: Login failed`) //This is to show that the 3rd attempt failed
+                console.log('Account locked due to too many failed login attempts') //This is to show that the account is locked
                 readline.close();
             }
         }
     }
 
-    readline.question(`Welcome, ${userInfo.userName}! Please type your password`, (inputPassword) => {
-        passwordAttempt = inputPassword;
+    readline.question(`Welcome, ${userInfo.userName}! Please type your password`, (inputPassword) => {/*This is the question when the 
+        user starts the program*/
+        passwordAttempt = inputPassword; //This is the variable used to store the passwordAttempt
 
-        loginAttempt(passwordAttempt)
+        loginAttempt(passwordAttempt) //Here I call the arrow function 
     })
 }
 
-createLoginTracker(userInfo);
+createLoginTracker(userInfo); //Here I call the createLoginTracker function
